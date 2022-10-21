507b88bb70cdc04938d2b0f0f2d9f9f903aece09
MirzadS <mirzad.smoloo@gmail.com>
2022-10-21 14:58:06 +0200

    Create RegisterPage & RegisterForm Component

---
+++ frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { RegisterForm } from "../components/forms/RegisterForm";
+import { Page } from "../utils/styles";
+
+export const RegisterPage = () => {
+  return (
+    <Page>
+      <RegisterForm />
+    </Page>
+  );
+};
